99f37a52e01edef0b69345d13216fa092898597f
Pedro Faustino <pedrobandim@gmail.com>
2012-12-31 22:22:44 +0100

    Add code. Add simple test.

--- test/icons_test.js
+++ test/icons_test.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var icons = require('../lib/icons.js');
+var icons = require('../lib/icons.js'),
+    express = require('express'),
+    app = express(),
+    fs = require('fs');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +25,35 @@ var icons = require('../lib/icons.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+app.get('/link', function(req, res){
+    var str = '<html><head>'+
+        '<link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-114.png" />' +
+        '</head><body></body></html>';
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    });
+    res.write(str, 'utf8');
+    res.end();
+});
+app.listen(6767);
+
+exports['discover'] = {
     setUp: function(done) {
         // setup here
         done();
     },
-    'no args': function(test) {
+    'apple-touch-icon on markup': function(test) {
         test.expect(1);
         // tests here
-        test.equal(icons.awesome(), 'awesome', 'should be awesome.');
-        test.done();
+        icons.discover("http://localhost:6767/link", function(err, devices) {
+            var icons = 0;
+            for (var i = 0; i < devices.length; i++) {
+                for (var j = 0; j < devices[i].urls.length; j++) {
+                    icons++;
+                }
+            }
+            test.equal(icons, 1, 'should have discovered 1 icon.');
+            test.done();
+        });
     }
 };
